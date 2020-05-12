6266ccc5082f8582c875e1c03a68b4fcbcdf2f53
Andreas Halvorsen Tollånes <andreas@tollanes.com>
2020-05-12 23:31:30 +0200

    added some preliminary tests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,17 +14,7 @@ import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AdminComponent } from './admin/admin.component';
 import { AuthModule } from './auth/auth.module';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
-
-const config = {
-  apiKey: 'AIzaSyAcTjgqq0ifpF5Hjcz334aiuHTxNztbN6I',
-  authDomain: 'noroff-projectexam-02.firebaseapp.com',
-  databaseURL: 'https://noroff-projectexam-02.firebaseio.com',
-  projectId: 'noroff-projectexam-02',
-  storageBucket: 'noroff-projectexam-02.appspot.com',
-  messagingSenderId: '156222630589',
-  appId: '1:156222630589:web:d3bbe08c66eff461fd25d4',
-  measurementId: 'G-H8SZ64J4QX',
-};
+import { firebaseConfig } from './app.config';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, AdminComponent, NavBarComponent],
@@ -32,7 +22,7 @@ const config = {
     BrowserModule,
     AppRoutingModule,
     AuthModule,
-    AngularFireModule.initializeApp(config),
+    AngularFireModule.initializeApp(firebaseConfig),
     AngularFirestoreModule,
     AngularFireAuthModule,
     AngularFireStorageModule,
