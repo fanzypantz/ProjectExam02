efc2c8050720ad1154be457512733e6902bfb879
Andreas Halvorsen Tollånes <andreas@tollanes.com>
2020-05-15 02:44:36 +0200

    made the config a bit more simpler
    no need for an entire new object for the layout really

--- src/app/admin/admin.config.ts
+++ src/app/admin/admin.config.ts
@@ -1,37 +1,35 @@
 export const adminConfig = {
   establishments: {
     readLayout: [
-      { name: 'id', layout: { type: 'string', key: 'id' } },
-      { name: 'images', layout: { type: 'imageArray', key: 'imageUrl' } },
+      { name: 'id', type: 'string', key: 'id' },
+      { name: 'images', type: 'imageArray', key: 'imageUrl' },
       {
         name: 'booking status',
-        layout: {
-          type: 'booking',
-          keys: ['bookingStart', 'bookingEnd'],
-        },
+        type: 'booking',
+        keys: ['bookingStart', 'bookingEnd'],
       },
-      { name: 'name', layout: { type: 'string', key: 'establishmentName' } },
-      { name: 'price', layout: { type: 'string', key: 'price' } },
-      { name: 'max guests', layout: { type: 'string', key: 'maxGuests' } },
-      { name: 'last updated', layout: { type: 'date', key: 'updatedAt' } },
+      { name: 'name', type: 'string', key: 'establishmentName' },
+      { name: 'price', type: 'string', key: 'price' },
+      { name: 'max guests', type: 'string', key: 'maxGuests' },
+      { name: 'last updated', type: 'date', key: 'updatedAt' },
     ],
   },
   enquiries: {
-    readLayout: [{ name: 'id', layout: { type: 'string', key: 'id' } }],
+    readLayout: [{ name: 'id', type: 'string', key: 'id' }],
   },
   messages: {
-    readLayout: [{ name: 'id', layout: { type: 'string', key: 'id' } }],
+    readLayout: [{ name: 'id', type: 'string', key: 'id' }],
   },
   posts: {
-    readLayout: [{ name: 'id', layout: { type: 'string', key: 'id' } }],
+    readLayout: [{ name: 'id', type: 'string', key: 'id' }],
   },
   users: {
     readLayout: [
-      { name: 'uid', layout: { type: 'string', key: 'uid' } },
-      { name: 'display name', layout: { type: 'string', key: 'displayName' } },
-      { name: 'email', layout: { type: 'string', key: 'email' } },
-      { name: 'images', layout: { type: 'image', key: 'photoURL' } },
-      { name: 'roles', layout: { type: 'roles', key: 'roles' } },
+      { name: 'uid', type: 'string', key: 'uid' },
+      { name: 'display name', type: 'string', key: 'displayName' },
+      { name: 'email', type: 'string', key: 'email' },
+      { name: 'images', type: 'image', key: 'photoURL' },
+      { name: 'roles', type: 'roles', key: 'roles' },
     ],
   },
 };
@@ -39,9 +37,7 @@ export const adminConfig = {
 // Just to give the config some type declaration
 export interface ReadInterface {
   name: string;
-  layout: {
-    type: string;
-    key?: string;
-    keys?: string[];
-  };
+  type: string;
+  key?: string;
+  keys?: string[];
 }
