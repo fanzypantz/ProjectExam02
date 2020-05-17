58d857ffad05c90adca9c46d507263a18d236933
Andreas Halvorsen Tollånes <andreas@tollanes.com>
2020-05-18 01:38:50 +0200

    added the components to routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-// Components
-import { AdminComponent } from './admin/admin.component';
-import { HomeComponent } from './home/home.component';
-
+// Guards
 import { CanDeleteGuard } from './auth/guards/can-delete.guard';
 import { CanReadGuard } from './auth/guards/can-read.guard';
 
+// Components
+import { AdminComponent } from './admin/admin.component';
+import { HomeComponent } from './home/home.component';
+import { AccommodationsComponent } from './accommodations/accommodations.component';
+import { ContactComponent } from './contact/contact.component';
+import { EnquiriesComponent } from './enquiries/enquiries.component';
+
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'accommodations', component: AccommodationsComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'enquiries', component: EnquiriesComponent },
   { path: 'admin', component: AdminComponent, canActivate: [CanDeleteGuard] },
 ];
 
