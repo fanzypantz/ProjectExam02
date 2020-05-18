b6263a015d9b28f72c758e09f3609b9c3037fa95
Andreas Halvorsen Tollånes <andreas@tollanes.com>
2020-05-18 23:42:26 +0200

    home gallery completed

--- src/app/home/gallery/gallery.component.ts
+++ src/app/home/gallery/gallery.component.ts
@@ -1,12 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
   styleUrls: ['./gallery.component.scss'],
 })
-export class GalleryComponent implements OnInit {
+export class GalleryComponent implements OnInit, OnDestroy {
+  public position = 0;
+  public innerWidth: any;
+  private interval: number;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.innerWidth = window.innerWidth;
+    this.interval = window.setInterval(() => {
+      if (this.position < 3) {
+        this.position++;
+      } else {
+        this.position = 0;
+      }
+    }, 10000);
+  }
+
+  goToImage(id) {
+    this.position = id;
+  }
+
+  ngOnDestroy() {
+    if (this.interval) {
+      clearInterval(this.interval);
+    }
+  }
 }
