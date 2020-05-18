0246f400ff08a2d9c0cf9b01ca3788e6069fe0bd
Andreas Halvorsen Tollånes <andreas@tollanes.com>
2020-05-18 23:46:42 +0200

    added some constraints to the animation

--- src/app/home/gallery/gallery.component.ts
+++ src/app/home/gallery/gallery.component.ts
@@ -9,22 +9,31 @@ export class GalleryComponent implements OnInit, OnDestroy {
   public position = 0;
   public innerWidth: any;
   private interval: number;
+  private isAnimating: boolean;
 
-  constructor() {}
+  constructor() {
+    this.isAnimating = false;
+  }
 
   ngOnInit(): void {
     this.innerWidth = window.innerWidth;
     this.interval = window.setInterval(() => {
       if (this.position < 3) {
-        this.position++;
+        this.goToImage(this.position + 1);
       } else {
-        this.position = 0;
+        this.goToImage(0);
       }
     }, 10000);
   }
 
   goToImage(id) {
-    this.position = id;
+    if (!this.isAnimating) {
+      this.isAnimating = true;
+      this.position = id;
+      setTimeout(() => {
+        this.isAnimating = false;
+      }, 1000);
+    }
   }
 
   ngOnDestroy() {
