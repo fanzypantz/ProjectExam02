bc3b3ed9538abd4c289a00942ff41e7f84cf1c89
Andreas Halvorsen Tollånes <andreas@tollanes.com>
2020-05-21 23:47:29 +0200

    posts styling mostly done

--- src/app/home/posts/posts.component.ts
+++ src/app/home/posts/posts.component.ts
@@ -1,12 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Router } from '@angular/router';
+import { Post } from '../../admin/shared/models/post.model';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.scss'],
 })
-export class PostsComponent implements OnInit {
-  constructor() {}
+export class PostsComponent implements OnInit, OnDestroy {
+  private documentSubscription: Subscription;
+  document: Observable<Array<Post>>;
+  data: any;
 
-  ngOnInit(): void {}
+  constructor(private afs: AngularFirestore, private router: Router) {}
+
+  ngOnInit(): void {
+    this.document = this.afs
+      .collection<Post>('posts', (ref) => ref.limit(4))
+      .valueChanges({ idField: 'id' });
+    this.documentSubscription = this.document.subscribe((snapshot) => {
+      this.data = snapshot;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.documentSubscription.unsubscribe();
+  }
+
+  goTo(id) {
+    this.router.navigate(['/accommodation-details', id]);
+  }
 }
