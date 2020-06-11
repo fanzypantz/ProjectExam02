f321b0a3bf4b55ff7e9f49d6c8950c785eb68837
Andreas Halvorsen Tollånes <andreas@tollanes.com>
2020-06-12 01:13:02 +0200

    nav menu now closes on route change

--- src/app/shared/nav-bar/nav-bar.component.ts
+++ src/app/shared/nav-bar/nav-bar.component.ts
@@ -1,24 +1,40 @@
-import { Component, HostListener, Input, OnInit } from '@angular/core';
+import {
+  Component,
+  HostListener,
+  Input,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
 import { AuthService } from '../auth/auth.service';
 import { PageTransitionsService } from '../page-transitions.service';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.scss'],
 })
-export class NavBarComponent implements OnInit {
+export class NavBarComponent implements OnInit, OnDestroy {
   @Input() isMobile: boolean;
+  private paramSub: Subscription;
   private isAnimating: boolean;
   public hideMenu: boolean;
 
   constructor(
     public auth: AuthService,
+    private router: Router,
     public pageTransition: PageTransitionsService
   ) {}
 
   ngOnInit(): void {
-    this.hideMenu = this.isMobile;
+    this.paramSub = this.router.events.subscribe((p) => {
+      this.hideMenu = true;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.paramSub.unsubscribe();
   }
 
   toggleMenu() {
