c625f32051837ae56c5ea5f7c9d200d1a28b838c
Andreas Halvorsen Tollånes <andreas@tollanes.com>
2020-06-12 12:27:34 +0200

    work on enquiries started

--- src/app/enquiries/enquiries.component.ts
+++ src/app/enquiries/enquiries.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { PageTransitionsService } from '../shared/page-transitions.service';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable, Subscription } from 'rxjs';
+import { AuthService } from '../shared/auth/auth.service';
+import { Enquiry } from '../admin/shared/models/enquiry.model';
+import { User } from '../admin/shared/models/user.model';
+import { Establishment } from '../admin/shared/models/establisment.model';
 
 @Component({
   selector: 'app-enquiries',
@@ -7,9 +13,37 @@ import { PageTransitionsService } from '../shared/page-transitions.service';
   styleUrls: ['./enquiries.component.scss'],
 })
 export class EnquiriesComponent implements OnInit {
-  constructor(private pageTransition: PageTransitionsService) {}
+  private userSubscription: Subscription;
+  private enquiriesSubscription: Subscription;
+  private collections: Observable<Enquiry[]>;
+  public data: Enquiry[];
+  private user: User;
+
+  constructor(
+    private afs: AngularFirestore,
+    private pageTransition: PageTransitionsService,
+    private auth: AuthService
+  ) {}
 
   ngOnInit(): void {
-    this.pageTransition.toggleOpenClose(500);
+    this.userSubscription = this.auth.user$.subscribe((userSnapshot) => {
+      this.user = userSnapshot;
+
+      this.collections = this.afs
+        .collection<Enquiry>('enquiries', (ref) =>
+          ref.where('email', '==', this.user.email)
+        )
+        .valueChanges({ idField: 'id' });
+
+      this.enquiriesSubscription = this.collections.subscribe((snapshot) => {
+        console.log('snapshot: ', snapshot);
+        this.data = snapshot;
+        this.pageTransition.toggleOpenClose(0);
+      });
+    });
+  }
+
+  formatDate(date) {
+    return `${date.getYear()}-${date.getMonth()}-${date.getDay()}`;
   }
 }
