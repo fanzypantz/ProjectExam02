a8b4819c1e4e766b61c0e8639ec0480dc3ffb7b5
Andreas Halvorsen Tollånes <andreas@tollanes.com>
2020-06-14 16:52:04 +0200

    fixed overflow on images in admin

--- src/app/admin/shared/read/images/images.component.ts
+++ src/app/admin/shared/read/images/images.component.ts
@@ -3,12 +3,14 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-read-images',
   template: `
-    <img
-      class="admin-form-img-small"
-      *ngFor="let image of images"
-      [src]="image"
-      alt=""
-    />
+    <div class="admin-form-img-small-container">
+      <img
+        class="admin-form-img-small"
+        *ngFor="let image of images"
+        [src]="image"
+        alt=""
+      />
+    </div>
   `,
   styleUrls: ['./images.component.scss'],
 })
